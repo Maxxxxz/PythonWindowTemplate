20a743c79236611b1e2de663db14dd7e496586b1
Michael Cooper <michaelcooper99@hotmail.com>
2019-07-15 20:50:39 -0400

    Updated template with Pages integration and hotkeys.

--- templateWindow.py
+++ templateWindow.py
@@ -1,40 +1,65 @@
 import tkinter as tk
 from tkinter import filedialog
+import Pages
+
+WIDTH = 400
+HEIGHT = 400
 
 class Application(tk.Frame):
 
     selectedFiles = []  #empty list of files
 
     def __init__(self, master=None):
-        master.title("Template")                                    #change title here
-        master.geometry("300x300")                                  #change window size here
-        master.resizable(width=False, height=False)                 #resizable?
+        master.title("Template")                                  #change title here
+        master.geometry("{}x{}".format(WIDTH, HEIGHT))            #change window size here
+        master.resizable(width=False, height=False)               #resizable?
         tk.Frame.__init__(self, master, relief=tk.GROOVE)
         self.menubar = tk.Menu(self)
-
-        self.menu = tk.Menu(self.menubar, tearoff=0)
-        self.addMenus(self.menu)
 
         self.contentFrame = tk.Frame(master, width=100, height=100)
         self.contentFrame.pack(anchor=tk.NW, fill=tk.BOTH, expand=True)
 
+        self.pages = []         # initialize pages list
+
         self.addContent(self.contentFrame)
+
+        self.menus = []         # initialize menus list
+
+        self.addMenus(self.menus)
+
+        for page in self.pages:     # place all pages
+            page.place(in_=self.contentFrame, x=0, y=0, relwidth=1, relheight=1)
+
+        self.pages[0].show()
 
         master.config(menu=self.menubar)
 
-    def addMenus(self, menu=None):  #add menu items here
-        self.menubar.add_cascade(label="File", menu=menu)
-        menu.add_command(label="Open", command=self.openFiles)
-        menu.add_command(label="DEBUG|Print Open Files", command=lambda: print(self.selectedFiles))
-        menu.add_command(label="Clear Selections", command=self.clearSelections)
-        menu.add_separator()
-        menu.add_command(label="Exit", command=self.master.quit)
+        self.bind_all("<Control-Key-0>", self.pages[0].show)    # main menu
+        self.bind_all("<Control-Key-1>", self.pages[1].show)    # Page 2
+
+    def addMenus(self, menus=None):  #add menu items here
+
+        self.menus.append(tk.Menu(self.menubar, tearoff=0))
+
+        self.menubar.add_cascade(label="File", menu=menus[0])
+        menus[0].add_command(label="Open", command=self.openFiles)
+        menus[0].add_command(label="DEBUG|Print Open Files", command=lambda: print(self.selectedFiles))
+        menus[0].add_command(label="Clear Selections", command=self.clearSelections)
+        menus[0].add_separator()
+        menus[0].add_command(label="Exit", command=self.master.quit)
+
+        menus.append(tk.Menu(self.menubar, tearoff=0))  # create dropdown menu View
+
+        self.menubar.add_cascade(label="Pages", underline=0, menu=menus[1])
+        menus[1].add_command(label="Main Menu", command=self.pages[0].lift, accelerator="Control+0")
+        menus[1].add_command(label="Page 1", command=self.pages[1].lift, accelerator="Control+1")
 
     def clearSelections(self):
         self.selectedFiles.clear()
 
     def addContent(self, contentFrame=None):                        #add content to contentFrame here
-        pass
+        self.pages.append(Pages.Menu(WIDTH))
+        self.pages.append(Pages.Page2(WIDTH))
         # self.lb = tk.Listbox(contentFrame, width=30)
         # self.lb.pack(anchor=tk.CENTER)
 
