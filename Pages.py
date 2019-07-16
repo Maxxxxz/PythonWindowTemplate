0ee1138f0481e7154d3b0e5c4123ccb42b1e6ca8
Michael Cooper <michaelcooper99@hotmail.com>
2019-07-15 20:49:56 -0400

    Created Pages class with base menu

---
+++ Pages.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+class Page(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+    def show(self, event=None):
+        self.lift()
+
+#   Main menu
+class Menu(Page):
+    def __init__(self, w):
+        Page.__init__(self)
+        label = tk.Label(self, text="Python Template Window")
+        label.place(x=(w/2), y=25, anchor="center")  # Place label at top of screen
+
+class Page2(Page):
+    def __init__(self, w):
+        Page.__init__(self)
+        label = tk.Label(self, text="Page 2")
+        label.place(x=(w/2), y=25, anchor="center")
